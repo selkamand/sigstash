4e0f34a081b7be2377f2ae64bd44d1b336aa282b
selkamand <sam.elkamand@gmail.com>
2023-11-14 11:26:46 +1100

    feat: addded function for reducing signature complexity

---
+++ R/SBS96_To_SBS6.R
@@ -0,0 +1,18 @@
+
+#' Reduce Signature Complexity
+#'
+#' Reduce complex signatures into simpler forms by converting 'channel' level signatures into 'type' level signatures Takes complex signatures like SBS96 and turns them into SBS6 signatures.
+#'
+#' @param signatures a sigverse signature collection
+#'
+#' @return a list conforming to sigverse signature collection format
+#'
+sig_reduce_complexity_using_type <- function(signatures){
+   lapply(signatures, \(sig){
+     df = stats::aggregate(fraction ~ type, data = sig, sum)
+     df[['channel']] <- df[['type']]
+     df <- tibble::tibble(df[c('type', 'channel', 'fraction')])
+     return(df)
+   })
+}
+
