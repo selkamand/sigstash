c0d7a2e19abd35f4ee2713e0f231ebe5e3856698
Sam El-Kamand <sam.elkamand@gmail.com>
2024-09-03 18:02:03 +1000

    feat: added ability to read and write signature collections to file

---
+++ tests/testthat/test-read_and_write.R
@@ -0,0 +1,29 @@
+test_that("sig_write works", {
+  signatures <- list(
+    sig1 = dplyr::tibble(
+      type = rep("A>G", 3L),
+      channel = c("A[A->G]G", "A[A->G]C", "A[A->G]T"),
+      fraction = c(0.4, 0.1, 0.5)
+    ),
+    sig2 = dplyr::tibble(
+      type = rep("A>G", 3L),
+      channel = c("A[A->G]G", "A[A->G]C", "A[A->G]T"),
+      fraction = c(0.4, 0.1, 0.5)
+    )
+  )
+
+  # RDS
+  outfile_rds = withr::local_file(.file = "test.Rds")
+  sig_write_signatures(signatures = signatures, filepath = outfile_rds, format = "rds")
+  expect_equal(sig_read_signatures(outfile_rds, format = "rds"), signatures)
+
+  # Tidy csv
+  outfile_tidy = withr::local_file(.file = "test.tidy.csv")
+  sig_write_signatures(signatures = signatures, filepath = outfile_tidy, format = "csv_tidy")
+  expect_equal(sig_read_signatures(filepath = outfile_tidy, format = "csv_tidy"), signatures)
+
+  # Wide CSV
+  outfile_wide = withr::local_file(.file = "test.wide.csv")
+  sig_write_signatures(signatures = signatures, filepath = outfile_wide, format = "csv_wide")
+  expect_snapshot_file(outfile_wide)
+})
