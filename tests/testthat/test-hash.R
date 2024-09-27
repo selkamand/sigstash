32654c55cd622817488228b3b1f18fc35d9b250e
Sam El-Kamand <sam.elkamand@gmail.com>
2024-09-28 00:51:01 +1000

    fix: test sig_identify_collection where mode='all'

--- tests/testthat/test-hash.R
+++ tests/testthat/test-hash.R
@@ -21,9 +21,16 @@ test_that("All signatures can be accurately checked using sig_identify_collectio
         # Load the dataset in the current format
         signatures <- sig_load(dataset_name, format = fmt)
 
-        # Check that the result from sig_identify_collection matches the dataset name
+        # Check that the result from sig_identify_collection matches the dataset name in hash mode
         expect_equal(
           sig_identify_collection(signatures, return = "name", method = "hash"),
+          dataset_name,
+          info = paste("Failed for dataset", dataset_name, "in format", fmt)
+        )
+
+        # Check that the result from sig_identify_collection matches the dataset name in 'all' mode
+        expect_equal(
+          sig_identify_collection(signatures, return = "name", method = "all"),
           dataset_name,
           info = paste("Failed for dataset", dataset_name, "in format", fmt)
         )
