601e88fe22ff01af57fe100fe5e136e1cfe7a8ca
Sam El-Kamand <sam.elkamand@gmail.com>
2024-09-30 21:38:27 +1000

    fix: auto-adds collection attributes when parsing
    (from files)
    
    resolves issue #9

--- R/read_and_write.R
+++ R/read_and_write.R
@@ -48,6 +48,9 @@ sig_write_signatures <- function(signatures, filepath, format = c("rds", "csv_ti
 #'   \item{\strong{"rds"}}{Reads the collection from an RDS file.}
 #'   \item{\strong{"csv_tidy"}}{Reads the collection from a tidy (long) CSV file and converts it to a list of signatures.}
 #' }
+#' @param collection_name A string specifying the signature collection name to be added as the 'collection_name' attribute
+#' of the parsed signature object.
+#' If NULL, name is guessed from the filename (before the first '.'). Ignored if `format = 'rds'`.
 #' @return A list of signatures in sigstash format.
 #' @export
 #'
@@ -57,18 +60,32 @@ sig_write_signatures <- function(signatures, filepath, format = c("rds", "csv_ti
 #' \dontrun{
 #' signatures <- sig_read_collection(filepath = "signatures.rds", format = "rds")
 #' }
-sig_read_signatures <- function(filepath, format = c("rds", "csv_tidy")) {
+sig_read_signatures <- function(filepath, format = c("rds", "csv_tidy"), collection_name=NULL) {
+
+  # Assertions
   assertions::assert_file_exists(filepath)
+  if(!is.null(collection_name)) assertions::assert_string(collection_name)
+
   format <- rlang::arg_match(format)
 
+  # If RDS serialisation
   if(format == "rds") {
     signatures <- readRDS(filepath)
     return(signatures)
-  } else if(format == "csv_tidy") {
+  }
+  else if(format == "csv_tidy") {
     df_tidy <- utils::read.csv(file = filepath, header = TRUE)
     signatures <- sig_collection_reformat_tidy_to_list(df_tidy)
+
+    # Add attributes to signature collection
+    if(is.null(collection_name)) {
+      collection_name <- sub(x=basename(filepath), pattern = "\\..*", "")
+      }
+    signatures <- add_collection_attributes(signatures, name = collection_name, format = "sigstash")
+
     return(signatures)
-  } else {
+  }
+  else {
     stop("Reading signature collections from format: ", format, " has not been implemented yet")
   }
 }
